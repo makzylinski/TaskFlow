6bf911033440fd96b0962a622ef2dd70c91643a5
Maks <makzylinski@gmail.com>
2026-09-22 22:20:12 +0200

    board component

---
+++ task-flow-frontend/src/app/components/board/board.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-board',
+  styleUrl: './board.scss',
+  templateUrl: './board.html',
+})
+export class Board imp {
+
+}
