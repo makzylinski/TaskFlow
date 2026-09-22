6a5ce5a16c2b99c1c3142a67f13f4cd227cea709
Maks <makzylinski@gmail.com>
2026-09-22 23:24:09 +0200

    task component

---
+++ task-flow-frontend/src/app/components/task/task.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-task',
+  styleUrl: './task.scss',
+  templateUrl: './task.html',
+})
+export class Task {}
