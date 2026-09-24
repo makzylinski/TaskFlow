25d6102b1839bec692a4cdf7b77785983244cd7f
Maks <makzylinski@gmail.com>
2026-09-24 11:41:15 +0200

    test controller

--- src/main/java/com/max/TaskFlow/controller/TaskController.java
+++ src/main/java/com/max/TaskFlow/controller/TaskController.java
@@ -2,9 +2,11 @@ package com.max.TaskFlow.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/tasks")
 @CrossOrigin(origins = "http://localhost:4200")
 public class TaskController {
 
