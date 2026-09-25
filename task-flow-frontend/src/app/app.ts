779eee442aac4df1eb41ec0b07cc3d9aa83c88db
Maks <makzylinski@gmail.com>
2026-09-25 13:38:35 +0200

    html structure

--- task-flow-frontend/src/app/app.ts
+++ task-flow-frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidePanel } from './components/side-panel/side-panel';
 import { Toolbar } from './components/toolbar/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Toolbar],
+  imports: [RouterOutlet, Toolbar, SidePanel],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
