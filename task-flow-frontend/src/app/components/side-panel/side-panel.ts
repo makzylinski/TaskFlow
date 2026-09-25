d559ad16ab397e36d23b544b16f9e1f20d42a60d
Maks <makzylinski@gmail.com>
2026-09-25 13:33:51 +0200

    side panel

---
+++ task-flow-frontend/src/app/components/side-panel/side-panel.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-side-panel',
+  styleUrl: './side-panel.scss',
+  templateUrl: './side-panel.html',
+})
+export class SidePanel {}
