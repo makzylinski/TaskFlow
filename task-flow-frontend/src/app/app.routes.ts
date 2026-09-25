059039410704f306710a22ded8a12d389cdeb81d
Maks <makzylinski@gmail.com>
2026-09-25 10:32:10 +0200

    added dashboard and routes

--- task-flow-frontend/src/app/app.routes.ts
+++ task-flow-frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Board } from './components/board/board';
+import { Dashboard } from './components/dashboard/dashboard';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'board', pathMatch: 'full' },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: Dashboard },
   { path: 'board', component: Board },
-  { path: '**', redirectTo: 'board' },
+  { path: '**', redirectTo: 'dashboard' },
 ];
